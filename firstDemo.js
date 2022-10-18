42dfa0f48eb1681d83c636305a00f6457261c64a
Emily Kirn <emilykirn@gmail.com>
2022-10-18 14:41:37 -0600

    initial commit

---
+++ firstDemo.js
@@ -0,0 +1,30 @@
+console.log("Hello World!");
+
+const firstName = 'Emily';
+
+let age = 30;
+
+//console.log(`My name is ${firstName} and I am ${age} years old`);
+
+//console.log('my name is ' + firstName + ' and I am ' + age + ' years old.') 
+
+let num1 = 12
+let num2 = 7
+
+//let sum = num1 + num2;
+//let diff = num1 - num2; 
+//let div = num1 / num2;
+//let prod = num1 * num2;
+
+//console.log(sum);
+//console.log(diff);
+//console.log(div);
+//console.log(prod);
+
+
+if (num1 > num2){
+    console.log(`num1 is bigger than num2`)
+} else {
+    console.log(`num1 is not bigger than num2`)
+}
+
